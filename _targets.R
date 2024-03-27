db59409117190a074a3a5edde851992ba6e47372
Eric Scott <scottericr@gmail.com>
2024-03-26 17:49:37 -0700

    update pipeline

--- _targets.R
+++ _targets.R
@@ -8,55 +8,34 @@ library(targets)
 library(tarchetypes) # Load other packages as needed.
 library(crew.cluster)
 
+# You'll need to replace this with your HPC group name.  You can discover the name by running `va` when logged into the HPC.
+hpc_group <- "my_group_name"
+
 # Set target options:
 tar_option_set(
   packages = c("tibble"), # Packages that your targets need for their tasks.
-  # format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  #   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  # 
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
   # Set other options as needed.
+  # To run on multiple workers using the UA HPC you need to set the controller
+  # to crew_controller_slurm(). This example controller uses 3 workers with 1
+  # CPU and 4GB memory per worker. Unfortunately, due to constrainsts of the UA
+  # HPC this must be run from *on* the HPC and therefore the `tls` argument to
+  # crew_controller_slurm() must remain as default.
   controller = crew.cluster::crew_controller_slurm(
     workers = 3,
-    seconds_idle = 120,
+    seconds_idle = 120, #time until workers are shut down after idle
     slurm_partition = "standard",
     slurm_time_minutes = 60, #wall time for each worker
-    slurm_log_output = "crew_log_%A.out",
-    slurm_log_error = "crew_log_%A.err",
+    slurm_log_output = "logs/crew_log_%A.out",
+    slurm_log_error = "logs/crew_log_%A.err",
     slurm_memory_gigabytes_per_cpu = 4,
     slurm_cpus_per_task = 1,
     script_lines = c(
-      "#SBATCH --account kristinariemer", #not sure if I did this right
+      paste0("#SBATCH --account ", hpc_group),
       "module load R"
+      #add additional lines to the SLURM job script as necessary here
       )
   )
+  #to test locally, comment the above controller out to use the default.
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -66,17 +45,24 @@ tar_source()
 # Replace the target list below with your own:
 tar_plan(
   tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100), z = rnorm(100))
-    #   format = "feather" # efficient storage of large data frames
+    data_raw,
+    tibble(x = rnorm(100), y = rnorm(100), z = rnorm(100))
   ),
   tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
+    data,
+    do_stuff(data_raw),
   ),
   tar_target(
-    name = model2,
-    command = coefficients(lm(y ~ x + z, data = data))
+    model1,
+    lm(y ~ x, data = data)
+  ),
+  tar_target(
+    model2,
+    lm(y ~ x + z, data = data)
+  ),
+  tar_target(
+    model_compare,
+    AIC(model1, model2)
   )
 )
 
