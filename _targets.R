f2a3ab19a1c96f61095e794b1bc1c8b8b5efd78b
Eric Scott <scottericr@gmail.com>
2024-04-10 14:27:52 -0700

    comments

--- _targets.R
+++ _targets.R
@@ -15,6 +15,7 @@ hpc_group <- "kristinariemer" #TODO maybe get this from .Renviron var instead
 # Detect whether you're on HPC & not with an Open On Demand session (which cannot submit SLURM jobs) and set appropriate controller
 slurm_host <- Sys.getenv("SLURM_SUBMIT_HOST")
 
+# If on HPC, use SLURM jobs for parallel workers
 if (grepl("hpc\\.arizona\\.edu", slurm_host) & !grepl("ood", slurm_host)) {
   controller <- crew.cluster::crew_controller_slurm(
     workers = 3,
@@ -31,7 +32,7 @@ if (grepl("hpc\\.arizona\\.edu", slurm_host) & !grepl("ood", slurm_host)) {
       #add additional lines to the SLURM job script as necessary here
     )
   )
-} else {
+} else { # If local or on OOD session, use multiple R sessions for workers
   controller <- crew::crew_controller_local(workers = 3, seconds_idle = 60)
 }
 
