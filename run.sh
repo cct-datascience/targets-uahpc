8574d2cb8ff90309d6a3f254eb1797a531bd3173
Eric Scott <scottericr@gmail.com>
2024-10-11 16:02:29 -0700

    set account

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem-per-cpu=5gb          
 #SBATCH --time=24:00:00
 #SBATCH --partition=standard
-#SBATCH --account=<youraccount>
+#SBATCH --account=kristinariemer #replace with your account
 #SBATCH -o logs/%x_%j.out
 
 # Load necessary modules
